361642c58739205b9f60999ba426a2c9e474df2b
diianekawti <diianekawti@gmail.com>
2026-03-30 13:25:57 +0700

    fix indentation in next.config.js for consistency

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const path = require('path')
 
 const nextConfig = {
-  outputFileTracingRoot: path.join(__dirname),
+    outputFileTracingRoot: path.join(__dirname),
 }
 
 module.exports = nextConfig
